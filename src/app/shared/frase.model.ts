55db5e09a0bb28b387b30c28612de832634ee454
danielts90 <danieltadeu2.dt@gmail.com>
2018-02-06 11:03:14 -0200

    Criação da classe Frase

---
+++ src/app/shared/frase.model.ts
@@ -0,0 +1,9 @@
+export class Frase {
+
+    constructor(public fraseEng: string, 
+                public frasePtBr: string){
+
+        this.fraseEng = fraseEng;
+        this.frasePtBr = frasePtBr;
+    }
+}
