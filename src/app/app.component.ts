79bf4d3a33bc95a881efd0ebe171fbe87bd94cdd
danielts90 <danieltadeu2.dt@gmail.com>
2018-02-07 01:20:53 -0200

    Conclusão do App1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,17 @@ import { Component } from '@angular/core';
 export class AppComponent {
   
   public jogoEmAndamento: boolean = true;
+  public tipoEncerramento: string;
 
   public encerrarJogo(tipo: string): void {
     console.log(tipo);
     this.jogoEmAndamento = false;
+    this.tipoEncerramento = tipo;
+  }
+
+  public reiniciarJogo(): void{
+    this.jogoEmAndamento = true;
+    this.tipoEncerramento = undefined;
   }
 
 }
--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -1,6 +1,16 @@
-import { Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/core';
-import { Frase } from '../shared/frase.model';
-import { FRASES } from './frases-mock';
+import {
+  Component,
+  OnInit,
+  EventEmitter,
+  Output,
+  OnDestroy
+} from '@angular/core';
+import {
+  Frase
+} from '../shared/frase.model';
+import {
+  FRASES
+} from './frases-mock';
 
 @Component({
   selector: 'app-painel',
@@ -17,43 +27,43 @@ export class PainelComponent implements OnInit, OnDestroy {
 
   public progresso = 0;
   public tentativas = 3;
-  
+
   @Output() public encerrarJogo = new EventEmitter();
   constructor() {
     this.atualizaRodada();
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   ngOnDestroy() {
     console.log('O componente painel foi destruído.');
   }
 
   atualizaResposta(resposta: Event): void {
-    this.resposta = ((<HTMLInputElement>resposta.target).value);
+    this.resposta = (( < HTMLInputElement > resposta.target).value);
   }
 
-   verificarReposta(): void {
-     if (this.rodadaFrase.frasePtBr === this.resposta) {
-        this.rodada++;
-        this.progresso += 25;
-      } else {
-        this.tentativas--;
-      }
-
-      if (this.tentativas === 4) {
+  verificarReposta(): void {
+    if (this.rodadaFrase.frasePtBr === this.resposta) {
+      this.rodada++;
+      this.progresso = this.progresso + (100 / this.frases.length);
+      if (this.rodada === 4) {
         this.encerrarJogo.emit('vitoria');
       }
-      if (this.tentativas === -1) {
-      this.encerrarJogo.emit('derrota');
-      }
       this.atualizaRodada();
-   }
+    } else {
+      this.tentativas--;
+    }
+    if (this.tentativas === -1) {
+      this.encerrarJogo.emit('derrota');
+    }
+  }
 
-   atualizaRodada(): void {
+  atualizaRodada(): void {
+    if (this.rodada < FRASES.length) {
       this.rodadaFrase = this.frases[this.rodada];
       this.resposta = '';
-   }
+    }
+  }
 
 }
